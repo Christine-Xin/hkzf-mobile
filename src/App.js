a377b69348016cc504339a43d3057599272512e6
赵鑫 <xin.zhao@datacvg.com>
2023-01-02 15:59:59 +0800

    增加路由和修改全局样式

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 // 根组件（配置路由信息）
+import React from 'react'
+import {BrowserRouter as Router, Routes,Route, Link} from 'react-router-dom'
+import { Button } from "antd-mobile";
+import Home from './pages/Home'
+import CityList from './pages/CityList';
 
 function App() {
   return (
-    <div className="App">
-      嗨
-    </div>
+    
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/home">首页</Link>
+          </li>
+          <li>
+            <Link to="/citylist">城市选择</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path='/home' element={<Home/>}></Route>
+          <Route path='/citylist' element={<CityList/>}></Route>
+        </Routes>
+      </div>
+      
+    </Router>
   );
 }
 
